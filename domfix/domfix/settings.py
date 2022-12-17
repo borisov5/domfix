02b073c90ca964e2589de445791a2b5900a728a4
Ivelin Borisov <borisov1990@gmail.com>
2022-12-17 19:35:30 +0200

    changing staticfiles_dirs in settings.py

--- domfix/domfix/settings.py
+++ domfix/domfix/settings.py
@@ -105,17 +105,17 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 # STATIC_URL = 'static/'
-#
-# STATICFILES_DIRS = (
+# STATICFILES_DIRS = [
 #     BASE_DIR / 'static',
-# )
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+# ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 

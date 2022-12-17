fba5cedba414832c035bf019bf063b88701f939a
Ivelin Borisov <borisov1990@gmail.com>
2022-12-17 18:58:38 +0200

    changing settings file

--- domfix/domfix/settings.py
+++ domfix/domfix/settings.py
@@ -103,9 +103,9 @@ USE_TZ = True
 # )
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    BASE_DIR / 'static'
-)
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
